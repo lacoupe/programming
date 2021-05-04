08e1aae9e275da37af272742d8be1f568bbc883b
atissot3 <79792193+atissot3@users.noreply.github.com>
2021-05-04 17:30:40 +0200

    Update account_class.py

--- account_class.py
+++ account_class.py
@@ -24,17 +24,35 @@ class Account:
         if t==0:
             return
         
-        int_rate = rates[t-1]
-        int_payment = self.last_balance_t*int_rate  #Yearly interest rates payment based on last year balance
-        print('Interests received/paid on last year (t = {} to t = {}) : CHF {}'.format(t-1, t, int_payment))
+        if self.name == 'Saving':
+            
+            print('SAVING ACCOUNT:')
+            
+            int_rate = rates[t-1]
+            int_payment = self.last_balance_t*int_rate  #Yearly interest rates payment based on last year balance
+            print('Interests received/paid on last year (t = {} to t = {}) : CHF {}'.format(t-1, t, int_payment))
+    
+            net_mov = self.net_movements_t #Net yearly withdrawals, deposits, transfers need to adjust
+            print('Net movements on last year (t = {} to t = {}) : CHF {}'.format(t-1, t, net_mov))
+            
+            #adjust balance for interests and movements:
+            self.balance_t += (int_payment + net_mov)
+            
+            print('Last year (t = {}) balance was : CHF {} \nThis year ( t= {}) balance is : CHF {}'.format(t-1, self.last_balance_t, t, self.balance_t))
+        
+        elif self.name == 'Current':
+            
+            print('CURRENT ACCOUNT:')
+            
+            net_mov = self.net_movements_t #Net yearly withdrawals, deposits, transfers need to adjust
+            print('Net movements on last year (t = {} to t = {}) : CHF {}'.format(t-1, t, net_mov))
+            
+            #adjust balance for interests and movements:
+            self.balance_t += net_mov
+            
+            print('Last year (t = {}) balance was : CHF {} \nThis year ( t= {}) balance is : CHF {}'.format(t-1, self.last_balance_t, t, self.balance_t))
 
-        net_mov = self.net_movements_t #Net yearly withdrawals, deposits, transfers need to adjust
-        print('Net movements on last year (t = {} to t = {}) : CHF {}'.format(t-1, t, net_mov))
-        
-        #adjust balance for interests and movements:
-        self.balance_t += (int_payment + net_mov)
-        
-        print('Last year (t = {}) balance was : CHF {} \nThis year ( t= {}) balance is : CHF {}'.format(t-1, self.last_balance_t, t, self.balance_t))
+            
         
         #Reset movements balance to zero for the new coming year
         self.net_movements_t = 0 
@@ -60,31 +78,33 @@ class Account:
 
 
 ################TEST######################
-     
-test_rates = rate_evolution_sample()
 
-print(test_rates[0:2])    
+if __name__ == "__main__":
 
-a1 = Account('Current', 20000)
-a2 = Account('Spare', 20000)
-
-a1.info()
-a2.info()
-
-
-#at t=1 : (1) withdraw 100 and deposit 200 -> Net = +100
-a1.withdraw(100)
-a1.deposit(200)
-
-a1.yearly_adjustments(1, test_rates)
-a2.yearly_adjustments(1, test_rates)
-
-
-#at t=2 : (1) transfers 300 ton (2) and withdraw 200 --> Net = -500 AND a2 deposit 100 --> Net = +400
-a1.withdraw(200)
-a1.transfer(a2,300)
-a2.deposit(100)
-
-a1.yearly_adjustments(2, test_rates)
-a2.yearly_adjustments(2, test_rates)
+    test_rates = rate_evolution_sample()
+    
+    print(test_rates[0:2])    
+    
+    a1 = Account('Current', 20000)
+    a2 = Account('Spare', 20000)
+    
+    a1.info()
+    a2.info()
+    
+    
+    #at t=1 : (1) withdraw 100 and deposit 200 -> Net = +100
+    a1.withdraw(100)
+    a1.deposit(200)
+    
+    a1.yearly_adjustments(1, test_rates)
+    a2.yearly_adjustments(1, test_rates)
+    
+    
+    #at t=2 : (1) transfers 300 ton (2) and withdraw 200 --> Net = -500 AND a2 deposit 100 --> Net = +400
+    a1.withdraw(200)
+    a1.transfer(a2,300)
+    a2.deposit(100)
+    
+    a1.yearly_adjustments(2, test_rates)
+    a2.yearly_adjustments(2, test_rates)
 
