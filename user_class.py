93c1be6fe69e934ac1eddf8a6b591485e5f7cb26
atissot3 <79792193+atissot3@users.noreply.github.com>
2021-05-11 15:13:34 +0200

    Update user_class.py

--- user_class.py
+++ user_class.py
@@ -98,6 +98,7 @@ class user:
         #Print Saving Decision Report
         self.report_single_sav_dec(sal_t, frac_saving, frac_consuming)
         
+        #BANK STUFFS
         #Print Bank Report & update accounts
         print('\n')
         print('------------------------------------------------------')
@@ -109,6 +110,17 @@ class user:
         self.c_account.transfer(self.s_account, frac_sal_sav) #Savings transfer
         self.c_account.yearly_adjustments(self.t, self.int_rates, disp=True) #Current Account Updates and report
         self.s_account.yearly_adjustments(self.t, self.int_rates, disp=True) #Saving Account Updates and report
+        
+        #INVESTMENTS STUFFS
+        #For each asset, update invest. value by P&L, print report
+        for asset in [self.e_inv, self.t_inv, self.n_inv, self.g_inv]:
+            asset.single_year_adj_1(self.t)
+        #Ask for possible new investments/sales depending on performance we display
+        self.long_invest_decision() #Long transactions 
+        self.short_invest_decision() #Short transactions
+        #Update investments depending on new transactions
+        for asset in [self.e_inv, self.t_inv, self.n_inv, self.g_inv]:
+            asset.single_year_adj_2(self.t)
 
     def run_cycle(self, n, frac_sav, frac_cons):
         """
@@ -135,16 +147,29 @@ class user:
             
             frac_sal_sav = frac_sav * sal_t #Part of the salary we want to save
             frac_sal_cons = frac_cons * sal_t #Part of the salary we use for consumption
-        
+            
+            #BANK STUFFS
             self.c_account.deposit(sal_t) #Salary deposit
             self.c_account.withdraw(frac_sal_cons) #Consumption
             self.c_account.transfer(self.s_account, frac_sal_sav) #Savings transfer
             self.c_account.yearly_adjustments(self.t, self.int_rates, disp=False) #Current Account Updates and report
             self.s_account.yearly_adjustments(self.t, self.int_rates, disp=False) #Saving Account Updates and report
- 
+            
+            
         #Print Accounts Reports
         self.c_account.compute_account_report(beg, end)
         self.s_account.compute_account_report(beg, end)
+        #New Transactions?
+
+        #INVESTMENT STUFFS
+        for asset in [self.e_inv, self.t_inv, self.n_inv, self.g_inv]:
+            asset.multi_year_adj(beg, n, disp=True)
+            
+        self.long_invest_decision() #Long transactions 
+        self.short_invest_decision() #Short transactions
+        
+        for asset in [self.e_inv, self.t_inv, self.n_inv, self.g_inv]:
+            asset.single_year_adj_2(self.t)
 
         
     ###################PRINT FUNCTIONS###################
@@ -179,28 +204,114 @@ class user:
         print('\n', info, '\n')
         print('------------------------------------------------------') 
         
+    
+    def portfolio_positions(self):
+        
+        print('\n')
+        print('------------------------------------------------------')
+        print('-----------------PORTFOLIO POSITIONS------------------')
+        print('------------------------------------------------------')
+        print('Eatcoin : CHF ', self.e_inv.balance_t + self.e_inv.net_movements_t)
+        print('------------------------------------------------------')
+        print('Teslo : CHF ', self.t_inv.balance_t + self.t_inv.net_movements_t)
+        print('------------------------------------------------------')
+        print('Nestlo : CHF ', self.n_inv.balance_t + self.n_inv.net_movements_t)
+        print('------------------------------------------------------')
+        print('Gold : CHF ', self.g_inv.balance_t + self.g_inv.net_movements_t)
+        print('------------------------------------------------------')
 
 
-    ###################INTERACTION FUNCTIONS###################
+
+    ##################DECISION FUNCTIONS##################
 
     def set_sav_dec(self):
         
-        saving_q = 'What will be the fraction of your salary you want to save every year ? \n(format : type 0.5 if you want to save 50% of your salary)'
-        frac_sav = input(saving_q)
+        #saving_q = 'What will be the fraction of your salary you want to save every year ? \n(format : type 0.5 if you want to save 50% of your salary)'
+        #frac_sav = input(saving_q)
         
-        consuming_q = 'What will be the fraction of your salary you want to consume every year ? \n(format : type 0.5 if you want to consume 50% of your salary)'
-        frac_cons = input(consuming_q)
+        #consuming_q = 'What will be the fraction of your salary you want to consume every year ? \n(format : type 0.5 if you want to consume 50% of your salary)'
+        #frac_cons = input(consuming_q)
         
-        #frac_saving = 0.5
-        #frac_consuming = 0.5
+        frac_sav = 0.5
+        frac_cons = 0.5
         
         return frac_sav, frac_cons
     
+    def long_investment(self, loop = False):
+        
+        print('Assets available: Eatcoin, Teslo, Nestlo, Gold')
+        stock = input('Which asset do you want to invest in ? Type exact name')
+            
+        while stock not in ['Eatcoin', 'Teslo', 'Nestlo', 'Gold']:
+            stock = input('Type a valid asset name please ...')
+        
+        if stock == 'Eatcoin':
+            asset = self.e_inv
+        elif stock == 'Teslo':
+            asset = self.t_inv
+        elif stock == 'Nestlo':
+            asset = self.n_inv
+        elif stock == 'Gold':
+            asset = self.g_inv
+
+        print('Capital available: CHF ', self.s_account.balance_t)
+        amt = float(input('How much do you want to invest in this asset ?'))
+        while amt > self.s_account.balance_t:
+            amt = float(input('Enter a valid capital please ...'))
+
+        self.s_account.instant_adjustment(self.t, -amt)
+        asset.deposit(self.t, amt)
+        
+        if loop == True: self.long_invest_decision()
+
+    def short_investment(self, loop = False):
+        
+        print('Assets available: Eatcoin, Teslo, Nestlo, Gold')
+        stock = input('Which asset do you want to sell ? Type exact name')
+            
+        while stock not in ['Eatcoin', 'Teslo', 'Nestlo', 'Gold']:
+            stock = input('Type a valid asset name please ...')
+        
+        if stock == 'Eatcoin':
+            asset = self.e_inv
+        elif stock == 'Teslo':
+            asset = self.t_inv
+        elif stock == 'Nestlo':
+            asset = self.n_inv
+        elif stock == 'Gold':
+            asset = self.g_inv
+        
+        val = asset.balance_t + asset.net_movements_t
+        
+        print('Owned asset value: CHF ', val)
+        amt = float(input('How much do you want to sell in this asset ?'))
+        while amt > val:
+            amt = float(input('Enter a valid amount please ...'))
+
+        self.s_account.instant_adjustment(self.t, amt)
+        asset.withdraw(self.t, amt)
+        
+        if loop == True: self.short_invest_decision()
     
-    def invest_stock(self, name):
-        print()
+    def long_invest_decision(self):
+        
+        self.portfolio_positions()
+        
+        on = input('Do you want to invest money ? yes/no')
+        
+        if on == 'yes':         
+            self.long_investment(loop = True)
+        else:
+            return
+            
+    def short_invest_decision(self):
 
-
+        on = input('Do you want to sell some assets ? yes/no')
+        
+        if on == 'yes':         
+            self.short_investment(loop = True)
+        else:
+            return
 
 
 ####################TESTS######################
@@ -211,4 +322,7 @@ if __name__ == "__main__":
     antoine = user(25, 90000, 20000) 
     antoine.set_sav_dec()
     antoine.run_single_cycle() 
-    antoine.run_cycle(20, 0.5, 0.4) 
+    antoine.run_cycle(10, 0.5, 0.4) 
+    antoine.run_cycle(10, 0.5, 0.5)
+    #antoine.run_single_cycle() 
+
