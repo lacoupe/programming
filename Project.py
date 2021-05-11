91e287d19cd25094fa546539266c8f16f5e4e5a9
atissot3 <79792193+atissot3@users.noreply.github.com>
2021-05-11 17:41:06 +0200

    Update Project.py

--- Project.py
+++ Project.py
@@ -1,6 +1,16 @@
 
 import interactions
+from user_class import user
 
+#Start the game
 interactions.intro()
-interactions.get_info_salary()
-interactions.get_info_capital()
+
+#Get informations needed to start simulations
+age, salary_1 = interactions.get_info_salary()
+cap_0 = interactions.get_info_capital()
+
+#Initialize the game parameters
+player = user(age, salary_1, cap_0)
+
+#First year simulation
+player.run_single_cycle(interactions.first_simulation())
