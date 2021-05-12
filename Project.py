729ea6b384a346f415956ab635689dc1bd0749f1
atissot3 <79792193+atissot3@users.noreply.github.com>
2021-05-12 14:08:08 +0200

    Update Project.py

--- Project.py
+++ Project.py
@@ -2,15 +2,29 @@
 import interactions
 from user_class import user
 
+time = 0
+
 #Start the game
 interactions.intro()
 
 #Get informations needed to start simulations
-age, salary_1 = interactions.get_info_salary()
+age, salary_1, end_t = interactions.get_info_salary()
 cap_0 = interactions.get_info_capital()
 
 #Initialize the game parameters
 player = user(age, salary_1, cap_0)
 
 #First year simulation
-player.run_single_cycle(interactions.first_simulation())
+time = player.run_single_cycle(interactions.first_simulation())
+
+while time < end_t:
+    l, frac_s, frac_c = interactions.simulation_len_q(time, end_t)
+    
+    if l == 1:
+        player.run_single_cycle(frac_s, frac_c)
+    else:
+        player.run_cycle(l, frac_s, frac_c)
+    
+    time += l
+    
+
