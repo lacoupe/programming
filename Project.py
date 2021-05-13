53ba7997d7e74d64d8d673fb81d561a8de227617
atissot3 <79792193+atissot3@users.noreply.github.com>
2021-05-13 21:28:27 +0200

    Update Project.py

--- Project.py
+++ Project.py
@@ -15,16 +15,18 @@ cap_0 = interactions.get_info_capital()
 player = user(age, salary_1, cap_0)
 
 #First year simulation
-time = player.run_single_cycle(interactions.first_simulation())
+frac_s, frac_c = interactions.first_simulation()
+time = player.run_single_cycle(frac_s, frac_c, first_sim = True)
 
 while time < end_t:
     l, frac_s, frac_c = interactions.simulation_len_q(time, end_t)
-    
     if l == 1:
         player.run_single_cycle(frac_s, frac_c)
     else:
         player.run_cycle(l, frac_s, frac_c)
     
     time += l
-    
 
+interactions.end_word()
+player.end_report()
+player.end_reports_decision()
